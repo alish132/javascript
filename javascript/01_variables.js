13a0987a2997ef6cced95efcad3deb3369751dac
alish132 <111165322+alish132@users.noreply.github.com>
2024-06-08 15:57:40 +0000

    container change

---
+++ javascript/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 52314         /// Its value can not be change once it declared
+let accountEmail = 'alish@gmail.com'     // Its value can be change as many times as we want
+var accountNumber = 986272524  ///Do not use var. Always aviod this 
+accountState = 'Nepalgunj'    //This method also declared variable but it is bad practise so avoid this. 
+let accountName;       /// Declaring variable wihout value for future use.
+
+accountEmail = "harry@gmail.com"
+
+console.table([accountId,accountEmail,accountState,accountName])
