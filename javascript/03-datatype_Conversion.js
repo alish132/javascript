f62a53157b60ebdde8bbc1cd751a17c2096d2844
alish132 <111165322+alish132@users.noreply.github.com>
2024-06-09 03:45:17 +0000

    converting one datatype into another dataType

---
+++ javascript/03-datatype_Conversion.js
@@ -0,0 +1,31 @@
+// Conversion of other dataTypes in Number 
+let score = ""
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"20" => 20
+//"20abc" => NaN
+// true => 1 ; false => 0
+// null => 0
+// undefined => NaN
+
+
+
+// Conversion of other dataTypes in Boolean
+let loggedIn = "alish"
+let bool = Boolean(loggedIn)
+// console.log(typeof bool);
+// console.log(bool);
+
+// 1 => true; 0 => false;
+// "" => false
+// "alish" => true
+
+
+
+// Conversion of other dataType in String
+let num = 20
+let numString = String(num)
+console.log(typeof numString);
+console.log(numString);
