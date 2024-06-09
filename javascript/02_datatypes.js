e160eba6849c64f683350513764104fabb08f243
alish132 <111165322+alish132@users.noreply.github.com>
2024-06-09 01:59:13 +0000

    changes

---
+++ javascript/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict";   // This will treat all JS code as newer version
+
+alert(3+3)    // This can work in browser but it does not work in nodejs.
+
+
+// <--------------Data Types-------------------->
+
+// number => 2 to power 53
+// bigint =>  This is used to store big numbers which is greather than number
+// string => "This is string"
+// boolean => true/false
+// null => standlone value  // null means empty
+// undefined => value does not define
+//symbol => unique   // This is used to define components uniquely
