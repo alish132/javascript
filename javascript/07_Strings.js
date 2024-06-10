3d4f5be2da36a03c70c0bf54ef0e55427a09fc7c
alish132 <111165322+alish132@users.noreply.github.com>
2024-06-10 05:05:15 +0000

    Strings methods

---
+++ javascript/07_Strings.js
@@ -0,0 +1,27 @@
+const name = 'alish'
+const age = 20
+
+// console.log(`Hello my name is ${name} and i am ${age} year old`);
+
+const userName = 'Harry'
+
+// console.log(userName.length);
+// console.log(userName.toUpperCase());
+// console.log(userName.charAt(4));
+// console.log(userName.indexOf('r'));
+
+
+const userEmail = 'Elon@twitter.com'
+// console.log(userEmail.substring(0,4));
+// console.log(userEmail.slice(-6,-2));
+
+
+const nameSpace = '    alish    '
+// console.log(nameSpace);
+// console.log(nameSpace.trim());
+
+const user = "harry,india"
+console.log(user.replace(',', '-'));
+
+const address = 'Nepalgunj'
+console.log(address.includes('gunj'));
