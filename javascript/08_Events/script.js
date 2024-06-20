e043e58b7e206766bf5136015ea15000ed70c784
alish132 <111165322+alish132@users.noreply.github.com>
2024-06-20 07:25:55 +0000

    Event concepts

--- javascript/08_Events/script.js
+++ javascript/08_Events/script.js
@@ -12,7 +12,39 @@
 // clientX, clientY, screenX, screenY
 // altkey, ctrlkey, shiftkey, keycode
 
-document.getElementById('cat').addEventListener('click', function(e){
-    alert("This is cat")
-    console.log(e);
+// document.getElementById('cat').addEventListener('click', function(e){
+//     alert("This is cat")
+//     console.log(e);
+// })
+
+// ==========>  false (Bubble phase)   <=================
+// Inner eventlistener will implement first then after outer
+// document.getElementById('images').addEventListener('click', function(e){
+//     console.log('This inside ul');
+// },false)
+// document.getElementById('cat').addEventListener('click', function(e){
+//     console.log('This is cat');
+//     // e.stopPropagation()   // This will stop parent eventlistener
+// },false)
+
+// ===========> true (Capture phase)   <===================
+// Outer eventlistener will implement first then after inner
+// document.getElementById('images').addEventListener('click', function(e){
+//     console.log('This inside ul');
+// },true)
+// document.getElementById('cat').addEventListener('click', function(e){
+//     console.lkog('This is cat');
+// },true)
+
+
+
+// Removing image by simply clicking in that image.
+document.querySelector('#images').addEventListener('click', function(e){
+    console.log(e.target.parentNode);
+    let removeIt = e.target.parentNode
+    if(removeIt.tagName == 'LI'){
+        console.log('It is li');
+        removeIt.remove()
+    }
+
 })
